6a1c26b66aebccf026fac3e1908ddf13a7e751a0
Abdallah-Bola7 <56006714+Abdallah-Bola7@users.noreply.github.com>
2025-02-13 21:58:24 +0200

    Update AccountService.java

--- AccountService.java
+++ AccountService.java
@@ -12,5 +12,7 @@ public interface AccountService {
     // TODO create function with name deposit that return
     boolean deposit(Account account, double amount);
     boolean withdraw(Account account, double amount);
+    boolean transfer(Account sender, Account recipient, double amount);
+    Account findAccountByUsername(String username);
 
 }
